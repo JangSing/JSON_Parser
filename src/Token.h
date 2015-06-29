bd806778bf998a41cb0dbde4c4a1076927319b27
JangSing <jangsing94@live.com.my>
2015-06-30 00:14:30 +0800

    Edit the spacing of the Token.h file

--- src/Token.h
+++ src/Token.h
@@ -4,60 +4,60 @@
 #include <stdint.h>
 
 typedef enum {
-	TOKEN_UNKNOWN_TYPE,
-	TOKEN_INTEGER_TYPE,
-	TOKEN_FLOAT_TYPE,
-	TOKEN_STRING_TYPE,
-	TOKEN_OPERATOR_TYPE,
-	TOKEN_IDENTIFIER_TYPE,
+  TOKEN_UNKNOWN_TYPE,
+  TOKEN_INTEGER_TYPE,
+  TOKEN_FLOAT_TYPE,
+  TOKEN_STRING_TYPE,
+  TOKEN_OPERATOR_TYPE,
+  TOKEN_IDENTIFIER_TYPE,
 } TokenType;
 
 typedef enum {
-	PREFIX, //-2
-	INFIX,	//2*2
-	POSTFIX	//x++
+  PREFIX, //-2
+  INFIX,	//2*2
+  POSTFIX	//x++
 } Arity;
 
 typedef enum {
-	NONE,
-	LEFT_TO_RIGHT,
-	RIGHT_TO_LEFT,
+  NONE,
+  LEFT_TO_RIGHT,
+  RIGHT_TO_LEFT,
 } Associativity;
 
 typedef struct {
-	TokenType type;
+  TokenType type;
   uint32_t startColumn;
   uint32_t length;
 } Token;
 
 typedef struct {
-	TokenType type;
+  TokenType type;
   uint32_t startColumn;
   uint32_t length;
-	int value;
+  int value;
 } IntegerToken;
 
 typedef struct {
-	TokenType type;
+  TokenType type;
   uint32_t startColumn;
   uint32_t length;
-	double value;
+  double value;
 } FloatToken;
 
 typedef struct {
-	TokenType type;
+  TokenType type;
   uint32_t startColumn;
   uint32_t length;
-	char *name;
+  char *name;
 } IdentifierToken, StringToken;
 
 typedef struct {
-	TokenType type;
+  TokenType type;
   uint32_t startColumn;
   uint32_t length;
-	char *symbol;
-	Arity arity;
-	Token *token[0];
+  char *symbol;
+  Arity arity;
+  Token *token[0];
 } OperatorToken;
 
 Token *getToken();
